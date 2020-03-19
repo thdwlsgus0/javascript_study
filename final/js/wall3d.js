0b91bac25bbdff9fa99b48573922c492ced9d9bf
송진현 <songjinhyeon94@gmail.com>
2020-03-19 13:41:27 +0900

    javscript 스크롤 추가

--- final/js/wall3d.js
+++ final/js/wall3d.js
@@ -1,10 +1,21 @@
 (function(){
  
     const houseElem = document.querySelector('.house');
-    let maxScrollValue = document.body.offsetHeight - window.innerHeight;
-    
+    const barElem = document.querySelector('.progress-bar');
+    let maxScrollValue = null;
+
+    function resizeHandler(){
+        maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    }
     window.addEventListener('scroll', function(){
-        const zMove = pageYOffset / maxScrollValue * 1000 - 490;
+        const scrollPer = pageYOffset / maxScrollValue ;
+        const zMove = scrollPer* 1000 - 490; 
         houseElem.style.transform = "translateZ("+ zMove + 'vw)';
+
+        // progress bar
+        barElem.style.width = scrollPer*100+ '%';
+
     });
+    window.addEventListener('resize', resizeHandler);
+    resizeHandler();
 })();
